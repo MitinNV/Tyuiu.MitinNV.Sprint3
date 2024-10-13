72050598ad41c7e39ba07a2d45f41228866c2b7f
MitinNV <tyuiu.mitinnv@outlook.com>
2024-10-13 22:42:24 +0500

    .

---
+++ Tyuiu.MitinNV.Sprint3.Task6.V15.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.MitinNV.Sprint3.Task6.V15.Lib;
+
+namespace Tyuiu.MitinNV.Sprint3.Task6.V15.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var varForTest = 168;
+            Assert.AreEqual(varForTest, ds.GetSumTheDivisors(6, 15));
+        }
+    }
+}
