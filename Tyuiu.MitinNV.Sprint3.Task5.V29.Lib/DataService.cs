ce897468267d0e7dafbc72f8439b21a802775c41
MitinNV <tyuiu.mitinnv@outlook.com>
2024-10-13 22:10:26 +0500

    .

--- Tyuiu.MitinNV.Sprint3.Task5.V29.Lib/DataService.cs
+++ Tyuiu.MitinNV.Sprint3.Task5.V29.Lib/DataService.cs
@@ -7,9 +7,9 @@ namespace Tyuiu.MitinNV.Sprint3.Task5.V29.Lib
         public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
         {
             var res = 0.0;
-            for (var i = startValue2; i <= stopValue2; i++)
+            for (var i = startValue1; i <= stopValue1; i++)
             {
-                for (var j = startValue1; j <= stopValue1; j++)
+                for (var j = startValue2; j <= stopValue2; j++)
                 {
                     res += (x / j) + Math.Sin(x);
                 }
